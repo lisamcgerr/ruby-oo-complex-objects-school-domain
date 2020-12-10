7087954bb3076234ed7507a196879cb890d05671
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2020-12-09 23:25:13 -0600

    commit tree 235215bc674341affdbfccba6b828ec6658aaf93

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = Hash.new
+    end
+
+    def add_student(student_name, grade)
+        if @roster.keys.any?(grade)
+        @roster[grade] << student_name
+        else 
+        @roster[grade] = [ ]
+        @roster[grade] << student_name
+        end
+    end
+    
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.values.flatten.sort
+    end
+
+end
+
+school = School.new("Bayside High School")
